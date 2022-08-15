cca048dff4b5e85eb8b1f8c9677dbea00516e4c6
Source Academy User <No public email provided>
2022-08-15 23:54:34 +0800

    Changes made from Source Academy

---
+++ cs1101s first playground.js
@@ -0,0 +1,28 @@
+// Type your program in here!
+const x = 3;
+
+const b = 1>3;
+
+true ? 1+1:17;
+
+x>0? 23:36;
+
+x===3? 5:7;
+
+true ? 312 : 2 < 1 ? 4711 : 42 ? 321:312;
+
+function square(x){
+    return x*x;
+}
+
+square(21);
+
+function hypotenuse(a,b){
+    return math_sqrt(a*a + b*b);
+}
+
+hypotenuse(3,4);
+
+1+2*3+4;
+
+1 + 2 >= 3 ? 4 * 5 : 6 + 7;
