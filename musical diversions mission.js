f5288bebb59b14314996b376f0e67799a4afd9e2
nichee <45154216+nichee@users.noreply.github.com>
2022-09-18 18:32:42 +0800

    Update musical diversions mission.js

--- musical diversions mission.js
+++ musical diversions mission.js
@@ -1,6 +1,12 @@
 function snare_drum(t) {
-    play(adsr(0.2, 0.3, 0.3, 0.1)(noise_sound(t)));
+    return (adsr(0.2, 0.3, 0.3, 0.1)(noise_sound(t)));
 
 }
 
-snare_drum(0.5);
+//play(snare_drum(0.5));
+
+
+function bass_drum(t){
+    return (adsr(0.2, 0.3, 0.3, 0.1)(sine_sound(79,t)));
+}
+play(bass_drum(0.5));
