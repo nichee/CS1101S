75680d49b3dd88722e1d9c9bbc4304f4172fff01
nichee <45154216+nichee@users.noreply.github.com>
2022-09-18 18:53:49 +0800

    Update musical diversions mission.js

--- musical diversions mission.js
+++ musical diversions mission.js
@@ -1,3 +1,5 @@
+//argument t in functions is time
+
 function snare_drum(t) {
     return (adsr(0.2, 0.3, 0.3, 0.1)(noise_sound(t)));
 
@@ -11,8 +13,11 @@ function bass_drum(t){
     const bassPrimeList = list(79,83,89,97,101,103,107,109,113,127,131
     ,137,139,149);
     
+    function sineConstructor(frequency){
+        return (sine_sound(frequency,t));
+    }
     
-    
-    return (adsr(0, 0.2, 0.11, 0)(sine_sound(149,t)));
+    const finalBass = simultaneously(map(sineConstructor,bassPrimeList));
+    return (adsr(0, 0.2, 0.11, 0)(finalBass));
 }
 play(bass_drum(0.5));
