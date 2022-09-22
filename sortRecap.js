d37716a8ff2318488c08f7e1c465b88f235e16c6
Source Academy User <No public email provided>
2022-09-22 21:51:12 +0800

    Changes made from Source Academy

--- sortRecap.js
+++ sortRecap.js
@@ -1,9 +1,26 @@
 /*
-mergeSort
+
+1)insertionSort
+Sort the tail of the given list using wishful thinking!
+Insert the head in the right place
+
+
+2)selectionSort
+Find the smallest element x and remove it from the list
+Sort the remaining list, and put x in front
+
+
+
+3)mergeSort
+Split the list in half, sort each half using wishful thinking
+Merge the sorted lists together
+
 take, drop, merge (two sorted lists into one sorted list), mergeSort(halve list,
 sort both halves and merge
 
-
+4)quickSort
+Split list into 2 subArrays, smaller and larger (than pivot size).
+Sort each subarray using wishful thinking 
 
 
 
