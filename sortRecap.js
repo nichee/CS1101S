d94c8a2d9246d189ba910619d6aaa5f5134b4974
Source Academy User <No public email provided>
2022-09-22 23:04:18 +0800

    Changes made from Source Academy

--- sortRecap.js
+++ sortRecap.js
@@ -23,4 +23,35 @@ Split list into 2 subArrays, smaller and larger (than pivot size).
 Sort each subarray using wishful thinking 
 
 
+*/
+
+//insert element into sorted array
+function insert(element, array){ 
+    if (is_null(array)) {
+        return array;
+    }
+    else {
+        return element <= head(array)
+            ? pair(element, array)
+            : pair(head(array), insert(element, tail(array)));
+    }
+}
+display (insert(5,list(3,4)));
+
+
+
+
+function insertionSort(array){
+    return length(array) === 1
+        ? array
+        : insert(head(array), insertionSort(tail(array)));
+}
+
+
+
+
+
+
+
+
 
