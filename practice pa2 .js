7c3fd91479de14a5647d114b4dc9d2674e38045f
Source Academy User <No public email provided>
2022-11-09 00:22:57 +0800

    Changes made from Source Academy

---
+++ practice pa2 .js
@@ -0,0 +1,22 @@
+//qn 2b-------------------------
+function runlength_decode(R) {
+
+    // WRITE YOUR SOLUTION HERE.
+
+}
+
+runlength_decode(list(6, pair(5,2), 9, pair(7,2), pair(5,3)));
+// returns list(6,5,5,9,7,7,5,5,5)
+
+
+
+
+
+
+
+
+
+
+
+
+
