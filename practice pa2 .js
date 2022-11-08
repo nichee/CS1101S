db17bd2e61e7714e2797c1d1d712fa49eeace2a8
Source Academy User <No public email provided>
2022-11-09 00:39:55 +0800

    Changes made from Source Academy

--- practice pa2 .js
+++ practice pa2 .js
@@ -3,9 +3,26 @@ function runlength_decode(R) {
     if (length(R) <2){
         return R;
     }
-
-
+    
+    let finalList= list();
+    for (let i=0; i<length(R); i=i+1){
+        if (is_number(list_ref(R,i))){
+            finalList=append(finalList,list(list_ref(R,i)));
+        }
+        else if (is_pair(list_ref(R,i))){//is pair
+            let duplicates = list();
+            for (let j=0; j<tail(list_ref(R,i)); j=j+1){
+                duplicates = append(duplicates, list(head(list_ref(R,i))));
+                display(duplicates);
+            }
+            finalList=append(finalList,duplicates);
+            
+        }
+    }
+    return finalList;
 }
+
+
 
 runlength_decode(null);
 runlength_decode(list(9));
