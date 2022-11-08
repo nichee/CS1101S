f33e18fcf8538a67024aa46fb0ab97e6e3e95621
Source Academy User <No public email provided>
2022-11-09 01:47:18 +0800

    Changes made from Source Academy

--- mock PA.js
+++ mock PA.js
@@ -1,6 +1,11 @@
 function is_pa_word(s) {
     // your solution goes here
-    for (let i=0; i<length(pa_words), i=i+1)
+    for (let i=0; i<length(pa_words), i=i+1){
+        if(s === list_ref(pa_words,i)){
+            return true;
+        }
+    }
+    return false;
 }
 
 display(pa_words);
