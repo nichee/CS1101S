99f3052c33db55017f5696ff6ac53895abbb694f
Source Academy User <No public email provided>
2022-11-09 01:45:26 +0800

    Changes made from Source Academy

---
+++ mock PA.js
@@ -0,0 +1,9 @@
+function is_pa_word(s) {
+    // your solution goes here
+    for (let i=0, i<length)
+}
+display(pa_words);
+// testing
+
+// is_pa_word("exhilarating");   // should return true
+// is_pa_word("tintinnabulate"); // should return false
